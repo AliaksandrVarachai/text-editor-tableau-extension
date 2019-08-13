683f74093e311c994dd8be433006910059a7c817
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2019-08-13 21:37:22 +0300

    Add styles to started tour window

--- src/script/started-tour-window/started-tour.js
+++ src/script/started-tour-window/started-tour.js
@@ -1,19 +1,30 @@
-import './started-tour.html';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DOMPurify from 'dompurify';
+
+import './started-tour.html';
 import { POP_IS_CLOSED, EXTENSION_ORIGIN } from '../constants/constants';
+import * as mock from '../../mock';
+import './started-tour.pcss';
 
 window.onbeforeunload = function() {
   window.postMessage(POP_IS_CLOSED, EXTENSION_ORIGIN);
 };
 
+
+DOMPurify.setConfig({
+  ADD_TAGS: ['iframe'],
+  ADD_ATTR: ['target'],
+});
+
 class App extends React.PureComponent {
+
+
   render() {
     return (
-      <div>
-        Result will be shown here
-      </div>
+      <div styleName="content"
+           dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(mock.htmlContent)}}
+      />
     );
   }
 }
